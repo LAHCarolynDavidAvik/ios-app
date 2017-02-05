c74e1c444c4990a034c78dfb760825ea82814fbd
Carolyn Duan <kirbyfan4546@gmail.com>
2017-02-04 22:08:56 -0800

    create detail view storyboard & configure table cells

--- LAHApp/Models/Transaction.swift
+++ LAHApp/Models/Transaction.swift
@@ -13,19 +13,22 @@ struct Transaction {
 	
 	let lender: User? // will be given as a string
 	let debtor: User? // ''
-	let quantity: Int // number of cents
+	let cents: Int // number of cents
 	let description: String
 	let createdAt: Date
 	let deadline: Date // will be int - num of milliseconds or whatever
 	let interest: Double
+	let id: String
+	let status: Status
 	
 	init(json: JSON) {
 		let lenderUsername = json["lender"].stringValue
 		let debtorUsername = json["debtor"].stringValue
 		
-		// TODO: wait for avik to do some api shet and then set lender & debtor to the actual users
+		// TODO: use login lmao he's gonna get rid of the password
 		
-		self.quantity = json["quantity"].intValue
+		self.cents = json["quantity"].intValue
+		
 		self.description = json["description"].stringValue
 		
 		let createdAtSecs = json["createdAt"].intValue/1000
@@ -36,10 +39,14 @@ struct Transaction {
 		
 		
 		self.interest = json["interest"].doubleValue
-		
+		self.id = json["id"].stringValue
 		self.lender = nil
-		self.debtor = nil
-		
+		self.debtor = nil // TODO: FIX THIS SHITTY SHIT
+		self.status = Status(rawValue: json["status"].intValue)!
 	}
 	
+}
+
+enum Status: Int {
+	case Pending = 0, Unpaid, Paid
 }
---
+++ LAHApp/Views/DebtDetailView/DetailViewTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  DetailViewTableViewController.swift
+//  LAHApp
+//
+//  Created by Carolyn DUan on 2/4/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewTableViewController: UITableViewController {
+	
+	// this depends on the "status".
+	var cellIdentifiers: [String] = ["bob", "name", "money", "date", "purpose"]
+	var user: User!
+	
+	var transaction: Transaction! {
+		didSet {
+			// configure cell identifiers and figure out the order and labels and shit.
+			configureIdentifiers(lender: transaction.lender!, debtor: transaction.debtor!) // TODO: DON'T NEED TO FORCE UNWRAP
+			self.tableView.reloadData()
+		}
+	}
+	
+	@IBOutlet weak var bobLabel: UILabel! // "you owe", "you paid", "owes you", "paid you"
+	@IBOutlet weak var nameLabel: UILabel! // DAVID SUN (shit. this is gonna have to be a text field too)
+	@IBOutlet weak var moneyLabel: UILabel! // $20.00
+	@IBOutlet weak var dateLabel: UILabel!
+	@IBOutlet weak var purposeLabel: UILabel!
+	
+	
+	private func configureIdentifiers(lender: User, debtor: User) {
+		// if somebody else owes me money.
+		let status = self.transaction.status
+		if (lender.username == self.user.username) {
+			// if unpaid
+			if (status == Status.Unpaid) {
+				self.bobLabel.text = "owes you"
+			} else {
+				self.bobLabel.text = "paid you"
+			}
+			self.cellIdentifiers[0] = "name"
+			self.cellIdentifiers[1] = "bob"
+		} else {
+			// lender = someone else, debtor = me
+			// i owe somebody else money. don't mess with cellIdentifiers array
+			if (status == Status.Unpaid) {
+				self.bobLabel.text = "you owe"
+			} else {
+				self.bobLabel.text = "you paid"
+			}
+		}
+		
+		self.nameLabel.text = lender.name
+		self.moneyLabel.text = convertToDollars(cents: self.transaction.cents)
+		self.dateLabel.text = "by \(formatDate(date:self.transaction.deadline))"
+		self.purposeLabel.text = self.transaction.description
+	}
+	
+	private func formatDate(date: Date) -> String {
+		return "dummy/date"
+	}
+	
+	private func convertToDollars(cents: Int) -> String{
+		let centR = cents%100
+		let centString = (centR < 10) ? "0\(centR)" : "\(centR)"
+		let dollars = cents/100
+		return "$\(dollars).\(centString)"
+	}
+	
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.cellIdentifiers.count // ????
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifiers[indexPath.row], for: indexPath)
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
